72243dd6739e61ffd86bd9fa35e29e0dac74fb73
zouyonghao <yonghaoz1994@gmail.com>
2019-10-25 11:44:26 +0800

    add netty dependency.

---
+++ src/main/java/cn/edu/tsinghua/sdfs/net/FileSaveHandler.java
@@ -0,0 +1,10 @@
+package cn.edu.tsinghua.sdfs.net;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+
+public class FileSaveHandler extends SimpleChannelInboundHandler<String> {
+    protected void channelRead0(ChannelHandlerContext channelHandlerContext, String s) throws Exception {
+
+    }
+}
