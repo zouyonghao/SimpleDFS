a7dde1a5d3a7b4da6d8f6867a1f1d86fceb28cb7
zouyonghao <yonghaoz1994@gmail.com>
2019-11-13 15:55:20 +0800

    TEST: invoke function.

--- src/main/java/cn/edu/tsinghua/sdfs/user/program/ScriptRunner.kt
+++ src/main/java/cn/edu/tsinghua/sdfs/user/program/ScriptRunner.kt
@@ -1,9 +1,25 @@
 package cn.edu.tsinghua.sdfs.user.program
 
 import org.jetbrains.kotlin.script.jsr223.KotlinJsr223JvmLocalScriptEngineFactory
+import javax.script.Invocable
 
 fun main() {
-    val factory = KotlinJsr223JvmLocalScriptEngineFactory().scriptEngine
-    factory.eval("println(\"hello world\")")
-    factory.eval("println(\"hello world\")")
+    val engine = KotlinJsr223JvmLocalScriptEngineFactory().scriptEngine
+    engine.eval("println(\"hello world\")")
+    engine.eval("println(\"hello world\")")
+
+    engine.eval("""
+fun reduce(list: List<Int>) = list.reduce(operation = { a, b -> a + b })
+""")
+    val invocable = engine as? Invocable
+
+    val data = listOf(1, 2, 3, 4)
+    val res = (invocable!!.invokeFunction("reduce", data)
+            ?: run {
+                println("reduce function not exist!")
+                return@main
+            })
+
+    println(res)
+
 }
