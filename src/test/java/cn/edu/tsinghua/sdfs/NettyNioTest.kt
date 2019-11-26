6b29606821b918eb0519dd6a319cbbbf8467a5f4
zouyonghao <yonghaoz1994@gmail.com>
2019-11-27 00:44:06 +0800

    ADD: test nio

---
+++ src/test/java/cn/edu/tsinghua/sdfs/NettyNioTest.kt
@@ -0,0 +1,44 @@
+package cn.edu.tsinghua.sdfs
+
+import io.netty.bootstrap.ServerBootstrap
+import io.netty.buffer.Unpooled
+import io.netty.channel.ChannelHandlerContext
+import io.netty.channel.ChannelInboundHandlerAdapter
+import io.netty.channel.ChannelInitializer
+import io.netty.channel.nio.NioEventLoopGroup
+import io.netty.channel.socket.SocketChannel
+import io.netty.channel.socket.nio.NioServerSocketChannel
+import io.netty.handler.codec.http.*
+import io.netty.handler.codec.http.HttpHeaderNames.CONTENT_LENGTH
+import io.netty.handler.codec.http.HttpHeaderNames.CONTENT_TYPE
+import org.junit.jupiter.api.Test
+
+class NettyNioTest {
+
+    @Test
+    fun startHttpServer() {
+        ServerBootstrap().apply {
+            group(NioEventLoopGroup(), NioEventLoopGroup(2))
+            channel(NioServerSocketChannel::class.java)
+            childHandler(object : ChannelInitializer<SocketChannel>() {
+                override fun initChannel(socketChannel: SocketChannel) {
+                    socketChannel.pipeline()
+                            .addLast(HttpServerCodec())
+                            .addLast(object : ChannelInboundHandlerAdapter() {
+                                override fun channelRead(ctx: ChannelHandlerContext, msg: Any?) {
+                                    println(Thread.currentThread())
+                                    if (msg is HttpRequest) {
+                                        println("hello")
+                                    }
+                                    val response = DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK, Unpooled.wrappedBuffer("abc".toByteArray()));
+                                    response.headers().set(CONTENT_TYPE, "text/plain")
+                                    response.headers().set(CONTENT_LENGTH, response.content().readableBytes());
+                                    ctx.channel().writeAndFlush(response)
+                                }
+                            })
+                }
+
+            })
+        }.bind(8088).channel().closeFuture().sync()
+    }
+}
