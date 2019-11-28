3a5814031d0b2011ed286bb70aaca585acf0bdda
zouyonghao <yonghaoz1994@gmail.com>
2019-11-28 09:46:30 +0800

    TEST: nio socket.

--- src/test/java/cn/edu/tsinghua/sdfs/NettyNioTest.kt
+++ src/test/java/cn/edu/tsinghua/sdfs/NettyNioTest.kt
@@ -1,43 +1,121 @@
 package cn.edu.tsinghua.sdfs
 
 import io.netty.bootstrap.ServerBootstrap
-import io.netty.buffer.Unpooled
+import io.netty.channel.Channel
 import io.netty.channel.ChannelHandlerContext
 import io.netty.channel.ChannelInboundHandlerAdapter
 import io.netty.channel.ChannelInitializer
+import io.netty.channel.DefaultFileRegion
 import io.netty.channel.nio.NioEventLoopGroup
 import io.netty.channel.socket.SocketChannel
 import io.netty.channel.socket.nio.NioServerSocketChannel
-import io.netty.handler.codec.http.*
-import io.netty.handler.codec.http.HttpHeaderNames.CONTENT_LENGTH
-import io.netty.handler.codec.http.HttpHeaderNames.CONTENT_TYPE
+import io.netty.handler.codec.LineBasedFrameDecoder
+import io.netty.handler.codec.string.StringDecoder
+import io.netty.handler.codec.string.StringEncoder
+import io.netty.handler.stream.ChunkedWriteHandler
+import io.netty.util.CharsetUtil
+import java.io.BufferedReader
+import java.io.File
+import java.io.FileReader
 
 class NettyNioTest {
 
     // @Test
     fun startHttpServer() {
+        var first = true
         ServerBootstrap().apply {
-            group(NioEventLoopGroup(), NioEventLoopGroup(2))
+            group(NioEventLoopGroup(1))
             channel(NioServerSocketChannel::class.java)
             childHandler(object : ChannelInitializer<SocketChannel>() {
                 override fun initChannel(socketChannel: SocketChannel) {
                     socketChannel.pipeline()
-                            .addLast(HttpServerCodec())
+                            // .addLast(HttpServerCodec())
+                            // .addLast(HttpObjectAggregator(65536))
+                            .addLast(StringEncoder(CharsetUtil.UTF_8))
+                            .addLast(LineBasedFrameDecoder(8192))
+                            .addLast(StringDecoder(CharsetUtil.UTF_8))
+                            .addLast(ChunkedWriteHandler())
                             .addLast(object : ChannelInboundHandlerAdapter() {
                                 override fun channelRead(ctx: ChannelHandlerContext, msg: Any?) {
-                                    println(Thread.currentThread())
-                                    if (msg is HttpRequest) {
+                                    println("${Thread.currentThread()} start, " +
+                                            "which channel id is ${ctx.channel().id()}, " +
+                                            "time is ${System.currentTimeMillis()}")
+                                    if (first) {
+                                        first = false
                                         println("hello")
+                                        // ctx.channel().writeAndFlush("you are the first client\n")
+                                        // val f = RandomAccessFile("test_file/numberFile", "rw")
+                                        // val response = DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK);
+                                        // response.headers().set(CONTENT_TYPE, "text/plain")
+                                        // response.headers().set(CONTENT_LENGTH, f.length())
+                                        // readAndWrite(ctx.channel())
+                                        // FileInputStream(File("")).channel.
+                                        // val f = RandomAccessFile("test_file/numberFile", "rw")
+                                        val f = File("test_file/numberFile")
+                                        ctx.channel().writeAndFlush(DefaultFileRegion(f, 0, f.length())).addListener {
+                                            println("write file finished")
+                                        }
+                                        // ctx.channel().writeAndFlush(ChunkedNioFile(f.channel)).addListener {
+                                        //     println("write file finished")
+                                        // }
+                                        // ctx.writeAndFlush(ChunkedFile(f, 1000_000_000))
+                                        //         .addListener {
+                                        //     println("write file finished")
+                                        // }
+                                        // ctx.channel().writeAndFlush(Files.readAllBytes(Paths.get("test_file/numberFile")))
+                                        // ctx.pipeline().addLast(object : ChannelInboundHandlerAdapter() {
+                                        //     override fun channelActive(ctx: ChannelHandlerContext) {
+                                        //         // readAndWrite(ctx.channel())
+                                        //     }
+                                        //
+                                        //     override fun channelRead(ctx: ChannelHandlerContext, msg: Any?) {
+                                        //         // val response = DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK, Unpooled.wrappedBuffer("abc".toByteArray()));
+                                        //         // response.headers().set(CONTENT_TYPE, "text/plain")
+                                        //         // response.headers().set(CONTENT_LENGTH, response.content().readableBytes());
+                                        //         // ctx.channel().writeAndFlush(ChunkedNioFile(RandomAccessFile("test_file/numberFile", "rw").channel))
+                                        //         // ctx.channel().writeAndFlush(Files.readAllBytes(Paths.get("test_file/numberFile")))
+                                        //         // readAndWrite(ctx.channel())
+                                        //     }
+                                        // })
+                                        return
+                                    } else {
+                                        println("Not first")
+                                        // val response = DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK, Unpooled.wrappedBuffer("abc".toByteArray()));
+                                        // response.headers().set(CONTENT_TYPE, "text/plain")
+                                        // response.headers().set(CONTENT_LENGTH, response.content().readableBytes())
+                                        // ctx.channel().writeAndFlush(response)
+                                        ctx.writeAndFlush("you are not the first client\n")
+                                        println("${Thread.currentThread()} finished this request, which channel id is ${ctx.channel().id()}, " +
+                                                "time is ${System.currentTimeMillis()}")
                                     }
-                                    val response = DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK, Unpooled.wrappedBuffer("abc".toByteArray()));
-                                    response.headers().set(CONTENT_TYPE, "text/plain")
-                                    response.headers().set(CONTENT_LENGTH, response.content().readableBytes());
-                                    ctx.channel().writeAndFlush(response)
+                                }
+
+                                override fun channelUnregistered(ctx: ChannelHandlerContext?) {
+                                    super.channelUnregistered(ctx)
+                                    println("Channel unregistered")
                                 }
                             })
                 }
 
             })
         }.bind(8088).channel().closeFuture().sync()
+    }
+
+    private fun readAndWrite(channel: Channel) {
+        val length = File("test_file/numberFile").length()
+        val f = BufferedReader(FileReader("test_file/numberFile"))
+        val result = StringBuilder()
+        var count = 0
+        while (true) {
+            f.readLine()?.apply {
+                // println("reading in thread ${Thread.currentThread()}")
+                // result.append(this)
+
+                channel.writeAndFlush(this)
+                count += this.length
+            } ?: break
+        }
+        println("$length, $count")
+        // channel.writeAndFlush(result)
     }
 }
